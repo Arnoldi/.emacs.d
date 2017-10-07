a883beaa9f8ca34fe0f5b535c9bea5e02c6fc1e6
Alexander Arnoldi <alexander.arnoldi@bshg.com>
2017-10-07 22:51:39 +0200

    Emacs init.el added

---
+++ init.el
@@ -0,0 +1,22 @@
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+)
+
+(require 'org)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-log-done t)
+(desktop-save-mode t)
+
+(global-set-key (kbd "C-d") 'duplicate-line)
+(global-set-key (kbd "<delete>") 'delete-char)
+
+(server-start)
